e9ad16540f8d436b313dd3aae0b4f791f8cfff6a
qwer9545 <49192194+qwer9545@users.noreply.github.com>
2023-12-20 08:56:05 +0900

    kangwon/java/231219_홀짝에_따라_다른값_반환하기/Solution181935
    신규: for문으로 출력하는 내용은 완성을 하였으나 이후 값 추가 부분에서 어려움을 겪었음, fof문 내 i의 홀짝 판별을 위해 if 조건을 추가로 사용하며 해결

--- kangwon/java/baekjun/Solution181935.java
+++ kangwon/java/baekjun/Solution181935.java
@@ -1,26 +1,29 @@
-/*
+/* 
+https://school.programmers.co.kr/learn/courses/30/lessons/181935
 양의 정수 n이 매개변수로 주어질 때, n이 홀수라면 n 이하의 
 홀수인 모든 양의 정수의 합을 return 하고 n이 짝수라면 n 이하의 
 짝수인 모든 양의 정수의 제곱의 합을 return 하는 solution 함수를 작성해 주세요.
 */
 package algo;
 
-class Solution181935 {
-    public static void main(String[] args) {
-
-
-    int answer1 = 0;
-    int answer2 = 0;
-    int n = 7;
-
-    if(n%2==0)    {
-        for (int i = 2; i < n; i * 2) {
-            answer1 += i * i;
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        if(n%2 == 0) {
+	        for (int i = 1; i <= n; i++) {
+	        	if(i % 2 == 0) {
+	            answer += i * i;
+	        	}
+	        }
+	    } else {
+	        for (int j = 1; j <= n; j++) {
+	        	if(j%2 != 0) {
+	            answer += j; //-2, -1, 3, 4, 5
+	        	}
+	        }
+        
         }
-    } else {
-        for (int j = 1; j < n; j++) {
-            answer2 += j;
-        }
+        return answer;
     }
-}
 }
