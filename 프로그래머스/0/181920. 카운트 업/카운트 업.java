715990d2a8b4e97a3c1f5b638cf949534a32a174
qwer9545 <49192194+qwer9545@users.noreply.github.com>
2024-08-24 15:44:10 +0900

    [level 0] Title: 카운트 업, Time: 0.03 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -1,7 +1,6 @@
 class Solution {
     public int[] solution(int start_num, int end_num) {
-        int size = end_num - start_num;
-        int[] answer = new int[size+1];
+        int[] answer = new int[end_num - start_num+1];
         
         for(int i=0; i<answer.length; i++){
            answer[i] += start_num+i;     
