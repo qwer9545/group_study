4c13bd69a3740e10b6278a8653e2562452011a73
qwer9545 <49192194+qwer9545@users.noreply.github.com>
2024-08-24 16:22:57 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.07 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -1,14 +1,18 @@
 class Solution {
     public int solution(String my_string, String is_suffix) {
-        // int answer = 0;
-        // String[] stli = new String[my_string.length()];
-        // for(int i =0; i<my_string.length();i++){
-        //     stli[i] = my_string.substring(i);
-        // }
-            if(my_string.endsWith(is_suffix)){
+        int answer = 0;
+        String[] stli = new String[my_string.length()];
+        for(int i =0; i<my_string.length();i++){
+            stli[i] = my_string.substring(i);
+            if(is_suffix.equals(stli[i])) {
                 return 1;
             }
-            else
-                return 0;
+        }
+            // if(my_string.endsWith(is_suffix)){
+            //     return 1;
+            // }
+            // else
+            //     return 0;
+        return 0;
     }
 }
