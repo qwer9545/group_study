3852fc6257c9b697f72dce8cb5131b55d7fb6541
qwer9545 <49192194+qwer9545@users.noreply.github.com>
2024-09-19 16:55:36 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 0.49 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = {};
+        
+        Arrays.sort(num_list);
+        
+        return Arrays.copyOfRange(num_list, 0, 5);
+    }
+}
